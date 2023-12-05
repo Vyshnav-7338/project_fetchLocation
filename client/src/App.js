39a5e7316e7d1655b54b97e84b861f0ad0764e2c
Vyshnav Prasannakumar <vyshnavprasannakumar200@gmail.com>
2023-12-05 20:01:00 +0530

    location

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [location, setLocation] = useState(null);
+  const [location, setLocation] = useState({
+    name: "",
+    latitude: null,
+    longitude: null,
+  });
 
-  const handleRegister = async () => {
-    try {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const { latitude, longitude } = position.coords;
-            setLocation({ type: "Point", coordinates: [longitude, latitude] });
-          },
-          (error) => {
-            console.error("Error getting location:", error.message);
+  const getLocation = async () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const { latitude, longitude } = position.coords;
+          try {
+            const response = await axios.get(
+              `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=YOUR_GOOGLE_MAPS_API_KEY`// YOUR_GOOGLE_MAPS_API_KEY
+            );
+
+            if (response.data.results.length > 0) {
+              const name = response.data.results[0].formatted_address;
+              setLocation({ latitude, longitude, name });
+            } else {
+              console.error("No results found for reverse geocoding.");
+            }
+          } catch (error) {
+            console.error("Error fetching location name:", error);
           }
-        );
-      } else {
-        console.error("Geolocation is not supported by this browser.");
-      }
-
-      console.log(location);
-      const response = await axios.post("http://localhost:3001/api/register", {
-        location,
-      });
-
-      console.log(response.data.message);
-    } catch (error) {
-      console.error("Error registering hub:", error);
+        },
+        (error) => {
+          alert("User Location is off", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
     }
   };
 
-  return (
-    <div>
-      <h1>Location Registration</h1>
+  const saveLocation = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/location",
+        location
+      );
+      console.log("Location saved:", response.data);
+    } catch (error) {
+      console.error("Error saving location:", error);
+    }
+  };
 
-      <button onClick={handleRegister}>Register</button>
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Location Tracker</h1>
+      {location.latitude && location.longitude && (
+        <div className="location-info">
+          <p>Name: {location.name}</p>
+          <p>Latitude: {location.latitude}</p>
+          <p>Longitude: {location.longitude}</p>
+          <button onClick={saveLocation}>Save Location</button>
+        </div>
+      )}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,57 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/location");
-const db = mongoose.connection;
-
-db.on("error", (error) => {
-  console.error("MongoDB connection error:", error);
-});
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
+mongoose.connect("mongodb://localhost/locationDB");
 
 const locationSchema = new mongoose.Schema({
-  location: {
-    type: { type: String },
-    coordinates: [Number],
-  },
+  name: String,
+  latitude: Number,
+  longitude: Number,
 });
 
-locationSchema.index({ location: "2dsphere" });
+const Location = mongoose.model("Location", locationSchema);
 
-const LocationsModel = mongoose.model("Location", locationSchema);
-
-app.post("/api/register", async (req, res) => {
+app.post("/api/location", async (req, res) => {
   try {
-    const { location } = req.body;
-
-    if (
-      !location ||
-      !location.coordinates ||
-      location.coordinates.length !== 2
-    ) {
-      return res.status(400).json({ error: "Invalid location data" });
-    }
-
-    const locations = new LocationsModel({ location });
-    await locations.save();
-
-    res.status(201).json({ message: "Location registered successfully" });
+    const { name, latitude, longitude } = req.body;
+    const newLocation = new Location({ name, latitude, longitude });
+    await newLocation.save();
+    res.status(201).json(newLocation);
   } catch (error) {
-    console.error("Error registering Location:", error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
